4f2ceec5e00c7052c14125521847625366ee9865
Svyatoslav <48492422+Volkonenkoff@users.noreply.github.com>
2020-12-12 18:58:46 +0300

    Update Source.cpp

--- PART2/Source.cpp
+++ PART2/Source.cpp
@@ -10,34 +10,29 @@ using namespace std;
 int main()
 {
     system("chcp 1251");
-    unsigned long N =  100000000;
+    long long N =  100000000;
     long double step = 1.0 / N;
-    unsigned long ch = 10 * 830805;
+    long long ch = 10 * 830805;
     long double sum = 0.0;
     long double x;
+    int cntThreads;
 
-#pragma omp parallel
-    {
-#pragma omp master
-        {
-            int cntThreads;
-            cout << "Введите кол-во потоков: ";
-            cin >> cntThreads;
-            omp_set_num_threads(cntThreads);
-            cout << "Количество потоков " << cntThreads << std::endl;
-        }
-    }
-    
+
+    cout << "Введите кол-во потоков: ";
+    cin >> cntThreads;
+           
+    cout << "Количество потоков " << cntThreads << std::endl;
+ 
     
     DWORD t1 = timeGetTime();
-#pragma omp parallel for schedule(dynamic,ch) private(x) reduction (+:sum)
-    {
-        for (unsigned long i = 0; i < N; i++)
+#pragma omp parallel for schedule(dynamic,ch) private(x) num_threads(cntThreads) reduction (+:sum)
+    
+        for (long long i = 0; i < N; i++)
         {
             x = (i + .5) * step;
             sum = sum + 4.0 / (1. + x * x);
         }
-    }
+    
     sum = sum * step;
     DWORD t2 = timeGetTime();
     cout << "Время: " << t2 - t1 << endl;
